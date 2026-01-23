72361e54070e2bbcce50f3b6bffb22e2c7188d33
Jesus <j.ramirezdibstoys@gmail.com>
2026-01-22 20:18:18 -0600

    Add FizzBuzz solution

--- util.py
+++ util.py
@@ -13,8 +13,23 @@ def is_palindrome(str_to_evaluate):         # Palindrome is a word that can be r
     return is_palindrome                    # Returns the response to the program to indicate whether it is a palindrome
 
 
-def is_fizzbuzz(number):
-    pass
+def is_fizzbuzz(number):                    # Classic exercise: if it is divisible by 3, return "Fizz"; if divisible by 5, return "Buzz"; if divisible by both, return "FizzBuzz"
+    try:
+        num = int(number)                   # Tries to execute the code; if an error occurs, it will jump to the except block
+    except (ValueError, TypeError):         # ValueError when it cannot be converted. TypeError when the type is incompatible.
+        return None                         # If the above occurs, then the function ends 
+                                            # SPECIAL CASE
+    if num == 0:                            # The test requires the result to be 0
+      return 0                              # Returns the same value as the response
+    
+    if num % 3 == 0 and num % 5 == 0:       # If both conditions are met, then return "FizzBuzz"
+        return "FizzBuzz"
+    elif num % 3 == 0:                      # If it is divisible by 3 and the remainder is 0
+        return "Fizz"                       # then return "Fizz" 
+    elif num % 5 == 0:                      # Same condition but with 5
+        return "Buzz"                       # Return "Buzz"
+    
+    return num                              # It is not the special case 0, not divisible by 3 or 5, so return the same value
 
 
 def is_prime(number):
